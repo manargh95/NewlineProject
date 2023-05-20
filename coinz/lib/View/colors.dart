cc5b947f7f91b6cbba3263f7423f17b87fda1e21
manargh95 <manar_gh95@outlook.com>
2023-05-20 23:26:59 +0300

    I create splash screen with getx

---
+++ coinz/lib/View/colors.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class myColorYellow {
+  MaterialColor mycolor = MaterialColor(0xffffd900, <int, Color>{
+    50: Color(0xffffd900),
+    100: Color(0xffffd900),
+    200: Color(0xffffd900),
+    300: Color(0xffffd900),
+    400: Color(0xffffd900),
+    500: Color(0xffffd900),
+    600: Color(0xffffd900),
+    700: Color(0xffffd900),
+    800: Color(0xffffd900),
+    900: Color(0xffffd900),
+  });
+}
+
+class myColorOrange {
+  MaterialColor mycolor = MaterialColor(0xffffaf00, <int, Color>{
+    50: Color(0xffffaf00),
+    100: Color(0xffffaf00),
+    200: Color(0xffffaf00),
+    300: Color(0xffffaf00),
+    400: Color(0xffffaf00),
+    500: Color(0xffffaf00),
+    600: Color(0xffffaf00),
+    700: Color(0xffffaf00),
+    800: Color(0xffffaf00),
+    900: Color(0xffffaf00),
+  });
+}
+
+class myColorPink {
+  MaterialColor mycolor = MaterialColor(0xfffb79b4, <int, Color>{
+    50: Color(0xfffb79b4),
+    100: Color(0xfffb79b4),
+    200: Color(0xfffb79b4),
+    300: Color(0xfffb79b4),
+    400: Color(0xfffb79b4),
+    500: Color(0xfffb79b4),
+    600: Color(0xfffb79b4),
+    700: Color(0xfffb79b4),
+    800: Color(0xfffb79b4),
+    900: Color(0xfffb79b4),
+  });
+}
+
+class myColorpurple {
+  MaterialColor mycolor = MaterialColor(0xff9f81ea, <int, Color>{
+    50: Color(0xff9f81ea),
+    100: Color(0xff9f81ea),
+    200: Color(0xff9f81ea),
+    300: Color(0xff9f81ea),
+    400: Color(0xff9f81ea),
+    500: Color(0xff9f81ea),
+    600: Color(0xff9f81ea),
+    700: Color(0xff9f81ea),
+    800: Color(0xff9f81ea),
+    900: Color(0xff9f81ea),
+  });
+}
+
+class myColorBlue {
+  MaterialColor mycolor = MaterialColor(0xff58c4d8, <int, Color>{
+    50: Color(0xff58c4d8),
+    100: Color(0xff58c4d8),
+    200: Color(0xff58c4d8),
+    300: Color(0xff58c4d8),
+    400: Color(0xff58c4d8),
+    500: Color(0xff58c4d8),
+    600: Color(0xff58c4d8),
+    700: Color(0xff58c4d8),
+    800: Color(0xff58c4d8),
+    900: Color(0xff58c4d8),
+  });
+}
+
+class myColorDarkBlue {
+  MaterialColor mycolor = MaterialColor(0xff4890da, <int, Color>{
+    50: Color(0xff4890da),
+    100: Color(0xff4890da),
+    200: Color(0xff4890da),
+    300: Color(0xff4890da),
+    400: Color(0xff4890da),
+    500: Color(0xff4890da),
+    600: Color(0xff4890da),
+    700: Color(0xff4890da),
+    800: Color(0xff4890da),
+    900: Color(0xff4890da),
+  });
+}
+
+class myColorGreen {
+  MaterialColor mycolor = MaterialColor(0xff47e546, <int, Color>{
+    50: Color(0xff47e546),
+    100: Color(0xff47e546),
+    200: Color(0xff47e546),
+    300: Color(0xff47e546),
+    400: Color(0xff47e546),
+    500: Color(0xff47e546),
+    600: Color(0xff47e546),
+    700: Color(0xff47e546),
+    800: Color(0xff47e546),
+    900: Color(0xff47e546),
+  });
+}
+
+class myColorTurquoise {
+  MaterialColor mycolor = MaterialColor(0xff05dfb2, <int, Color>{
+    50: Color(0xff05dfb2),
+    100: Color(0xff05dfb2),
+    200: Color(0xff05dfb2),
+    300: Color(0xff05dfb2),
+    400: Color(0xff05dfb2),
+    500: Color(0xff05dfb2),
+    600: Color(0xff05dfb2),
+    700: Color(0xff05dfb2),
+    800: Color(0xff05dfb2),
+    900: Color(0xff05dfb2),
+  });
+}
